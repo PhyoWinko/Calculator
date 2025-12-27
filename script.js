c26255e213c334e387d417353b581df45757855d
PhyoWinko <phyowinko123@gmail.com>
2025-12-27 23:49:49 +0630

    added js, added some basic function basic function like add, subtract, multiply and divide

---
+++ script.js
@@ -0,0 +1,20 @@
+// some basic calculator function
+function add(a, b) {
+    answer = a + b;
+    console.log(answer);
+}
+
+function subtract(a, b) {
+    answer = a - b;
+    console.log(answer);
+}
+
+function multiply(a, b) {
+    answer = a * b;
+    console.log(answer);
+}
+
+function divide(a, b) {
+    answer = a / b;
+    console.log(answer);
+}
