d69191125ed92a5581ceb37d41cf297cd61f6f48
PhyoWinko <phyowinko123@gmail.com>
2026-02-07 18:55:49 +0630

    new css new logic testing in script.js

--- script.js
+++ script.js
@@ -21,45 +21,66 @@ function divide(a, b) {
 
 let firstNumber;
 let secondNumber;
-let operator;
-
+let currentOperator;
+let result;
 
 // operate funciton to call basic calculation function
 function operate(operand1, operand2, operator) {
-    if (operator === "+") {
-        add(operand1, operand2);
-    }
-    if (operator === "-") {
-        subtract(operand1, operand2);
-    }
-    if (operator === "*") {
-        multiply(operand1, operand2);
-    }
-    if (operator === "/") {
-        divide(operand1, operand2);
+    switch(operator) {
+        case "+":
+            add(operand1, operand2);
+            break;
+        case '-': 
+            subtract(operand1, operand2);
+            break;
+        case '*':
+            multiply(operand1, operand2);
+            break;
+        case '/':
+            divide(operand1, operand2);
+            break; 
     }
 }
 
-const display = document.querySelector('.display');
+const display = document.querySelector('.calculator-display');
 // displayText variable to show on display 
-let displayText = "";
-display.textContent = displayText;
+let displayinfo = {displayText: ""};
+console.log(`this is display Text ${displayinfo.displayText}`);
+display.textContent = displayinfo.displayText;
 // const buttonContainer = document.querySelector(".button-container");
 
 
 function updateDisplay(text) {
-    displayText += "" + String(text);
-    console.log(`displaytext: ${displayText}`);
-    display.textContent = displayText;
+    displayinfo.displayText += "" + String(text);
+    console.log(`Updated displaytext: ${displayinfo.displayText}`);
+    display.textContent = displayinfo.displayText;
 }
+
 
 const inputButtons = document.querySelectorAll('input');
 console.log(inputButtons);
 inputButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        console.log("click");
-        updateDisplay(this.value);
-    });
+    button.addEventListener('click', function(event) {
+        // this code suggested from copilot
+        const value = event.target.value;
+        const type = event.target.dataset.type;
+
+        console.log("checking status click", value, type);
+        console.log(event);
+        // display the value
+        updateDisplay(value);
+
+        if (type === "operator") {
+            console.log("operator button presssed!")
+            firstNumber = display.textContent.slice(0, -1);
+            console.log("first number", firstNumber);
+            currentOperator = value;
+        }
+
+        if (type === "answer") {
+            console.log("answer button pressed!");
+            result = operate(firstNumber, secondNumber, event.target.value);
+        }
+
+    })
 })
-
-
